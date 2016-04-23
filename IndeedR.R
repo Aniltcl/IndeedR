5a5cd5d68724216c48684ab4b41f915f57098643
thegeolawton <geofry.m.lawton@gmail.com>
2016-04-22 23:06:03 -0400

    Renamed Indeedgetresults() to indeedget25(), added indeedallresults() which loops thru get25 to get the whole list of results.

--- IndeedR.R
+++ IndeedR.R
@@ -1,4 +1,4 @@
-indeedgetresults <- function(query="",city="",state="",sort="relevance",radius=25,start=0,limit=10,maxage=NA,publisher=4013469286425094,version=2,format="XML",highlight=1,filter=1,latlong=0,country="US",useragent="Mozilla/%2F4.0%28Firefox%29"){
+indeedget25 <- function(query="",city="",state="",sort="relevance",radius=25,start=0,limit=25,maxage=NA,publisher=4013469286425094,version=2,format="XML",highlight=1,filter=1,latlong=0,country="US",useragent="Mozilla/%2F4.0%28Firefox%29"){
         queryurl <- "http://api.indeed.com/ads/apisearch?"
         #QuerySetup gets the important boring terms out of the way.
         queryurl <- paste(queryurl,"publisher=",publisher,"&v=",version,"&format=",format,sep="")
@@ -13,8 +13,23 @@ indeedgetresults <- function(query="",city="",state="",sort="relevance",radius=2
         
         tree <- xmlTreeParse(queryurl)
         root <- xmlRoot(tree)
-        frame <- xmlToDataFrame(getNodeSet(root,"//results/result"))
-        frame
+        result <- xmlToDataFrame(getNodeSet(root,"//results/result"))
+        result
         
+        
+}
+
+indeedallresults <- function(query="",city="",state="",sort="relevance",radius=25,start=0,limit=25,maxage=NA,publisher=4013469286425094,version=2,format="XML",highlight=1,filter=1,latlong=0,country="US",useragent="Mozilla/%2F4.0%28Firefox%29"){
+  result <- indeedget25(query,city,state,sort,radius,start,limit,maxage,publisher,version,format,highlight,filter,latlong,country,useragent)
+  frame <- data.frame()
+  frame <- rbind(frame,result)
+  
+  while(nrow(result)==limit){
+    #Sys.sleep(10)
+    result <- indeedget25(query,city,state,sort,radius,nrow(frame)+1,limit,maxage,publisher,version,format,highlight,filter,latlong,country,useragent)
+    frame <- rbind(frame,result)
+    
+  }
+  frame
 }
         
